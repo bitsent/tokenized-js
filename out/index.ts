59da7f2c05d5c85e92ecfd162df30d1d03ccd26c
Aleksandar Dinkov <alexander.n.dinkov@gmail.com>
2020-11-15 16:23:48 +0200

    removed 'out'

--- out/index.ts
+++
@@ -1,6 +0,0 @@
-import Actions from "./actions";
-import Assets from "./assets";
-import Messages from "./messages";
-import * as Base from "./Base";
-
-export default { Actions, Assets, Messages, Base };
